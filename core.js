de25a623be32f08b81014739216b060c1e0b1ee2
shirui <1229408499@qq.com>
2019-01-11 10:53:54 +0800

    format code

--- core.js
+++ core.js
@@ -1,98 +1,125 @@
 // cvs ,ctx, shape, posit, style, hooks
 
 function AnimaObj(initObj) {
-  var obj = this;
-  this.posit = {x:0,y:0};
-  this.shape = "unknow";
-  this.style = {fillStyle:"#fff",strokeStyle:"#000"};
-  this.creatTime = Date.now();
-  
-  Object.keys(initObj).forEach(function(key){
-    obj[key]=initObj[key];
-  })
+  var thisObj = this;
 
-  this.aliveTime = function(){return Date.now() - this.creatTime;};
-  this.getInfo = function(){return this.shape + ';alive:' + this.aliveTime() + 'ms;';};
-  this.hooks = {draw: smartDraw, move: smartMove, checkWall: checkWall, addSpeed: addSpeed};
+  // default value
+  this.posit = { x: 0, y: 0 };
+  this.shape = "unknow";
+  this.style = { fillStyle: "#fff", strokeStyle: "#000" };
+  this.creatTime = nowTime();
+  this.hooks = {
+    draw: smartDraw,
+    move: smartMove,
+    checkWall: checkWall,
+    addSpeed: addSpeed
+  };
+
+  Object.keys(initObj).forEach(function(key) {
+    thisObj[key] = initObj[key];
+  });
+
+  this.aliveTime = function() {
+    return nowTime() - this.creatTime;
+  };
+  this.getInfo = function() {
+    return this.shape + ";alive:" + this.aliveTime() + "ms;";
+  };
   this.runHook = function() {
-    var obj=this;
-    Object.keys(obj.hooks).forEach(function(key){
-      obj.hooks[key](obj);
-    })
-  }
-  
+    var thisObj = this;
+    Object.keys(thisObj.hooks).forEach(function(key) {
+      thisObj.hooks[key](thisObj);
+    });
+  };
+
   this.addHook = function(hook) {
-    var obj = this;
-    switch(typeof(hook)) {
+    var thisObj = this;
+    switch (typeof hook) {
       case "object":
-        if(isArray(hook)) {
-          for(var i=0;i<hook.length;i++){
-            if(typeof(hook[i]) == "function")
-              obj.hooks[hook[i].name]=hook[i];
-            else if(typeof(hook[i]) == "string")
-              obj.hooks[hook[i]]=window[hook[i]];
+        if (isArray(hook)) {
+          for (var i = 0; i < hook.length; i++) {
+            if (typeof hook[i] == "function")
+              thisObj.hooks[hook[i].name] = hook[i];
+            else if (typeof hook[i] == "string")
+              thisObj.hooks[hook[i]] = window[hook[i]];
           }
         } else {
-          Object.keys(hook).forEach(function(key){
-            obj.hooks[key]=hook[key];
-          });          
+          Object.keys(hook).forEach(function(key) {
+            thisObj.hooks[key] = hook[key];
+          });
         }
         break;
       case "function":
-        obj.hooks[hook.name]=hook;
+        thisObj.hooks[hook.name] = hook;
         break;
       case "string":
-        obj.hooks[hook]=window[hook];
+        thisObj.hooks[hook] = window[hook];
         break;
     }
-  }
-  
+  };
+
   this.removeHook = function(hook) {
-    var obj = this;
-    switch(typeof(hook)) {
+    var thisObj = this;
+    switch (typeof hook) {
       case "function":
-        Object.keys(obj.hooks).forEach(function(key){
-          if(obj.hooks[key] == hook)
-            delete obj.hooks[key];
-        })
+        Object.keys(thisObj.hooks).forEach(function(key) {
+          if (thisObj.hooks[key] == hook) delete thisObj.hooks[key];
+        });
         break;
       case "string":
-        delete obj.hooks[hook];
+        delete thisObj.hooks[hook];
         break;
     }
-  }
+  };
 }
 
+function getRandom(lower, upper, isInit) {
+  if (isInit) return parseInt(Math.random() * (upper - lower + 1) + lower, 10);
+  else return Math.random() * (upper - lower) + lower;
+}
 
+function getRandColor() {
+  return (
+    "rgb(" +
+    getRandom(0, 255, true) +
+    "," +
+    getRandom(0, 255, true) +
+    "," +
+    getRandom(0, 255, true) +
+    ")"
+  );
+}
+
+function nowTime() {
+  return Date.now();
+}
 
 function isArray(obj) {
-  return Object.prototype.toString.call(obj) === '[object Array]';;
+  return Object.prototype.toString.call(obj) === "[object Array]";
 }
-
 
 function clearCanvas(canvas) {
-  canvas.height=canvas.height;
+  canvas.height = canvas.height;
 }
 
-
 function smartDraw(obj) {
-  if(!obj)
-    obj=this;
-  if(!obj.shape || !obj.ctx || !obj.posit || !obj.style)
-    return false;
-  if(obj.style)
-    Object.keys(obj.style).forEach(function(key){
+  if (!obj) obj = this;
+  if (!obj.shape || !obj.ctx || !obj.posit || !obj.style) return false;
+
+  // plus style
+  if (obj.style)
+    Object.keys(obj.style).forEach(function(key) {
       obj.ctx[key] = obj.style[key];
-    })
-  
-  switch(obj.shape) {
+    });
+
+  switch (obj.shape) {
     case "unknow":
       break;
     case "line":
       break;
     case "font":
-      obj.ctx.strokeText(obj.posit.txt,obj.posit.x,obj.posit.y);
-      obj.ctx.fillText(obj.posit.txt,obj.posit.x,obj.posit.y);
+      obj.ctx.strokeText(obj.posit.txt, obj.posit.x, obj.posit.y);
+      obj.ctx.fillText(obj.posit.txt, obj.posit.x, obj.posit.y);
       return true;
     case "rect":
       obj.ctx.beginPath();
@@ -101,65 +128,59 @@ function smartDraw(obj) {
       break;
     case "arc":
       obj.ctx.beginPath();
-      obj.ctx.arc(obj.posit.x, obj.posit.y, obj.posit.r, 0, 2*Math.PI);
+      obj.ctx.arc(obj.posit.x, obj.posit.y, obj.posit.r, 0, 2 * Math.PI);
       obj.ctx.closePath();
       break;
   }
-  
+
   obj.ctx.fill();
   obj.ctx.stroke();
   return true;
 }
 
+function checkWall(obj) {
+  if (!obj) obj = this;
+  if (!obj.posit) return false;
 
-function checkWall(obj){
-  if(!obj)
-    obj=this;
-  if(!obj.posit)
-    return false;
-  
-  switch(obj.shape){
+  switch (obj.shape) {
     case "arc":
-      if(obj.posit.x-obj.posit.r<=0){
-        obj.posit.x=obj.posit.r;
-        obj.posit.vx=-obj.posit.vx;
+      if (obj.posit.x - obj.posit.r <= 0) {
+        obj.posit.x = obj.posit.r;
+        obj.posit.vx = -obj.posit.vx;
       }
-      if(obj.posit.x+obj.posit.r>=obj.cvs.width){
-        obj.posit.x=obj.cvs.width-obj.posit.r;
-        obj.posit.vx=-obj.posit.vx;
+      if (obj.posit.x + obj.posit.r >= obj.cvs.width) {
+        obj.posit.x = obj.cvs.width - obj.posit.r;
+        obj.posit.vx = -obj.posit.vx;
       }
-      if(obj.posit.y-obj.posit.r<=0){
-        obj.posit.y=obj.posit.r;
-        obj.posit.vy=-obj.posit.vy;
+      if (obj.posit.y - obj.posit.r <= 0) {
+        obj.posit.y = obj.posit.r;
+        obj.posit.vy = -obj.posit.vy;
       }
-      if(obj.posit.y+obj.posit.r>=obj.cvs.height){
-        obj.posit.y=obj.cvs.height-obj.posit.r;
-        obj.posit.vy=-obj.posit.vy;
+      if (obj.posit.y + obj.posit.r >= obj.cvs.height) {
+        obj.posit.y = obj.cvs.height - obj.posit.r;
+        obj.posit.vy = -obj.posit.vy;
       }
       break;
   }
 }
 
+function smartMove(obj) {
+  if (!obj) obj = this;
+  if (!obj.shape || !obj.ctx || !obj.posit) return false;
+  if (typeof obj.posit.vx == "undefined") return false;
 
-function smartMove(obj){
-  if(!obj)
-    obj=this;
-  if(!obj.shape || !obj.ctx || !obj.posit)
-    return false;
-  if(typeof(obj.posit.vx) == "undefined")
-    return false;
-
-  obj.posit.x+=obj.posit.vx;
-  obj.posit.y+=obj.posit.vy;
+  // the speed is milliseconds per pixel
+  // speed isn't depend on fps, i think this is more real
+  var passTime = nowTime() - (obj.lastTime || nowTime());
+  obj.posit.x += obj.posit.vx * passTime;
+  obj.posit.y += obj.posit.vy * passTime;
+  obj.lastTime = nowTime();
 }
 
+function addSpeed(obj) {
+  if (!obj) obj = this;
+  if (typeof obj.posit.ax == "undefined") return false;
 
-function addSpeed(obj){
-  if(!obj)
-    obj=this;
-  if(typeof(obj.posit.ax) == "undefined")
-    return false;
-  
-  obj.posit.vx*=obj.posit.ax;
-  obj.posit.vy*=obj.posit.ay;
+  obj.posit.vx *= obj.posit.ax;
+  obj.posit.vy *= obj.posit.ay;
 }
--- main.js
+++ main.js
@@ -7,43 +7,60 @@ canvas.width = window.innerWidth;
 window.onresize = function() {
   canvas.height = window.innerHeight;
   canvas.width = window.innerWidth;
-}
+};
 
 function animaStart(cvs, ctx) {
-  var now=Date.now();
-  var frameCount=0;
-  var fps=0;
-  var FPS=new AnimaObj({
+  var now = Date.now();
+  var frameCount = 0;
+  var fps = 0;
+  var FPS = new AnimaObj({
     cvs: cvs,
     ctx: ctx,
     shape: "font",
-    posit: {txt:"FPS:"+fps,x:2,y:20},
-    style: {fillStyle:"#0f0",strokeStyle:"#0f0",font:"20px Verdana"},
-    hooks: {draw: smartDraw},
+    posit: { txt: "FPS:" + fps, x: 2, y: 20 },
+    style: { fillStyle: "#0f0", strokeStyle: "#0f0", font: "20px Verdana" },
+    hooks: { draw: smartDraw }
   });
-  var ball=new AnimaObj({
+  var ball = new AnimaObj({
     cvs: cvs,
     ctx: ctx,
     shape: "arc",
-    posit: {x:300,y:300,r:10,vx:2,vy:1,ax:1.001,ay:1.001},
-    style: {fillStyle:"#f00",strokeStyle:"#000"},
+    posit: { x: 300, y: 300, r: 10, vx: 0.2, vy: 0.1, ax: 1.001, ay: 1.001 },
+    style: { fillStyle: getRandColor(), strokeStyle: "#000" }
+  });
+  var rect = new AnimaObj({
+    cvs: cvs,
+    ctx: ctx,
+    shape: "rect",
+    posit: {
+      x: 300,
+      y: 200,
+      width: 30,
+      height: 30,
+      vx: 0.3,
+      vy: 0.1,
+      ax: 1.001,
+      ay: 1.0001
+    },
+    style: { fillStyle: getRandColor(), strokeStyle: "#000" }
   });
 
-  function iter(){
+  function iter() {
     clearCanvas(cvs);
-    
+
     ball.runHook();
+    rect.runHook();
 
     //calc the fps
-    if(Date.now()-now>=100) {
-      fps=(1000/((Date.now()-now)/frameCount)).toFixed(0);
-      frameCount=0;
-      now=Date.now();
+    if (Date.now() - now >= 100) {
+      fps = (1000 / ((Date.now() - now) / frameCount)).toFixed(0);
+      frameCount = 0;
+      now = Date.now();
     }
-    FPS.posit.txt="FPS:"+fps;
+    FPS.posit.txt = "FPS:" + fps;
     FPS.runHook();
     frameCount++;
-    
+
     requestAnimationFrame(iter);
   }
   requestAnimationFrame(iter);
